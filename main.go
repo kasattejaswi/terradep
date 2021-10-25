b71322a3b54e6e67d6d47388e4855d5636733e7c
Tejaswi Kasat <kasattejasvi@gmail.com>
2021-10-25 17:02:09 +0530

    Added command line cobra

--- main.go
+++ main.go
@@ -1,24 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"log"
-
-	"github.com/kasattejaswi/terradep/deps"
-)
-
-var runFlag bool
+import "github.com/kasattejaswi/terradep/cmd"
 
 func main() {
-	flag.Parse()
-	d, err := deps.ReadDcf("dependencyConfigs.yaml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(d.Dependencies)
-}
-
-func init() {
-	flag.BoolVar(&runFlag, "run", false, "Start resolving dependencies. It must run in the root module")
+	cmd.Execute()
 }
